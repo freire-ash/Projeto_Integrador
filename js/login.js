a85dccd61f9bac48b4c477fd477ecf79ad018c29
Isabelle Nascimento <noisabelle06@gmail.com>
2024-05-13 07:10:59 -0300

    estilizando login

--- js/login.js
+++ js/login.js
@@ -1,48 +1,40 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    var email = document.getElementById("email").value;
-    var senha = document.getElementById("senha").value;
-    var confirmacaoSenha = document.getElementById("Csenha").value;
 
-    // Verifica se todos os campos estão preenchidos
-    if (!email || !senha || !confirmacaoSenha) {
-        alert("Por favor, preencha todos os campos.");
-        event.preventDefault();
-        return;
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+function validarFormulario() {
+    var nome = document.getElementById('nome').value;
+    var idade = document.getElementById('idade').value;
+    var cpf = document.getElementById('cpf').value;
+    var email = document.getElementById('signup-email').value;
+    var senha = document.getElementById('signup-password').value;
+    var senhaConfirmar = document.getElementById('signup-password-confirm').value;
+
+
+    if (!validarEmail(email)) {
+        alert('Email inválido');
+        return false;
     }
 
-    if (!emailValido(email)) {
-        alert("Por favor, insira um email válido.");
-        event.preventDefault();
-        return;
+    if (!senhasIguais(senha, senhaConfirmar)) {
+        alert('As senhas não são iguais');
+        return false;
     }
 
-    if (!senhaValida(senha)) {
-        alert("A senha deve conter pelo menos 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.");
-        event.preventDefault();
+    if (!todasInformacoesPreenchidas(nome, idade, cpf, email, senha, senhaConfirmar)) {
+        alert('Por favor, preencha todas as informações');
+        return false;
     }
-});
 
-function emailValido(email) {
-    // Expressão regular para validar email
-    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
-function senhaValida(senha) {
-    //  8 caracteres
-    if (senha.length < 8) return false;
-
-    //  maiúscula
-    if (!/[A-Z]/.test(senha)) return false;
-
-    //  minúscula
-    if (!/[a-z]/.test(senha)) return false;
-
-    //  número
-    if (!/[0-9]/.test(senha)) return false;
-
-    //  caractere especial
-    if (!/[^A-Za-z0-9]/.test(senha)) return false;
+    if (!verificarForcaSenha(senha)) {
+        alert('Senha fraca. Use uma senha com pelo menos 8 caracteres.');
+        return false;
+    }
 
     return true;
 }
